93acdfb24633cf6398a0e0476c5862e3417d7024
Окулова Василиса <ok.vasilisa@yandex.ru>
2026-04-09 00:05:29 +0300

    Создание класса Report, хранящего название, содержание и дату составления отчёта

---
+++ Report.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace report_management
+{
+    public class Report
+    {
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public DateTime CreationDate { get; set; }
+
+        public Report(string title, string content, DateTime creationDate)
+        {
+            Title = title;
+            Content = content;
+            CreationDate = creationDate;
+        }
+    }
+}
