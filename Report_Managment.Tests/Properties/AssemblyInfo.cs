d6e3211e5e9c2c9cd8a1caa7eb8eace6338f8f33
Vasilisa <ok.vasilisa@yandex.ru>
2026-04-23 15:27:06 +0300

    Добавление тестов к классу Report

---
+++ Report_Managment.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Report_Managment.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Report_Managment.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2026")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("ef0a4788-ad46-412c-8c79-00fead4ea8fd")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Report_Managment.Tests/ReportTests.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using report_management;
+using System;
+
+namespace Report_Management.Tests
+{
+    //+ тесты
+    [TestClass]
+    public class ReportTests
+    {
+        [TestMethod]
+        public void Constructor_SetsTitleAndContentAndDate()
+        {
+            // Подготовка
+            string title = "Мой отчёт";
+            string content = "Текст отчёта...";
+            DateTime date = new DateTime(2026, 4, 23);
+
+            // Действие
+            Report report = new Report(title, content, date);
+
+            // Проверка
+            Assert.AreEqual(title, report.Title);
+            Assert.AreEqual(content, report.Content);
+            Assert.AreEqual(date, report.CreationDate);
+        }
+
+        [TestMethod]
+        public void CanChangeTitleAndContent()
+        {
+            // Подготовка
+            Report report = new Report("Старое", "Старое содержание", DateTime.Now);
+
+            // Действие
+            report.Title = "Новое название";
+            report.Content = "Новое содержание";
+
+            // Проверка
+            Assert.AreEqual("Новое название", report.Title);
+            Assert.AreEqual("Новое содержание", report.Content);
+        }
+    }
+}
